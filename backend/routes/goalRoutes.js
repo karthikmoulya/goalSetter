674c1ec18c5a5c22c42dbc73e5726167977578c3
karthikmoulya <karthikmoulya1995@gmail.com>
2022-06-17 08:19:24 +0530

    Added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,10 +5,11 @@ const {
   updateGoal,
   deleteGoal,
 } = require('../controller/goalController');
+const { protect } = require('../middleware/authMiddleWare');
 
 const router = express.Router();
 
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').delete(deleteGoal).put(updateGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal);
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e => {
+    e.preventDefault();
+
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = () => {};
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
